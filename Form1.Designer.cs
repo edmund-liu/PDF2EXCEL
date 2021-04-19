2a7b9a5a7b8e42bcfb98f34df73062094cce7654
edmund-liu <edmund.liu.xianlong@gmail.com>
2021-04-19 10:16:11 +0800

    This is the initial build.
    This is built on Visual Studio 2017. On C# for Windows application.

---
+++ Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace PDF2CSV
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnPDFLoc = new System.Windows.Forms.Button();
+            this.btnExcelLoc = new System.Windows.Forms.Button();
+            this.lblOutput = new System.Windows.Forms.Label();
+            this.lblInput = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(514, 161);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(200, 38);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Convert Now";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(128, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Source PDF File Location";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(31, 97);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(132, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Output Excel FIle Location";
+            // 
+            // btnPDFLoc
+            // 
+            this.btnPDFLoc.Location = new System.Drawing.Point(185, 30);
+            this.btnPDFLoc.Name = "btnPDFLoc";
+            this.btnPDFLoc.Size = new System.Drawing.Size(61, 23);
+            this.btnPDFLoc.TabIndex = 3;
+            this.btnPDFLoc.Text = "...";
+            this.btnPDFLoc.UseVisualStyleBackColor = true;
+            this.btnPDFLoc.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btnExcelLoc
+            // 
+            this.btnExcelLoc.Location = new System.Drawing.Point(185, 92);
+            this.btnExcelLoc.Name = "btnExcelLoc";
+            this.btnExcelLoc.Size = new System.Drawing.Size(61, 23);
+            this.btnExcelLoc.TabIndex = 4;
+            this.btnExcelLoc.Text = "...";
+            this.btnExcelLoc.UseVisualStyleBackColor = true;
+            this.btnExcelLoc.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // lblOutput
+            // 
+            this.lblOutput.AutoSize = true;
+            this.lblOutput.Location = new System.Drawing.Point(278, 97);
+            this.lblOutput.Name = "lblOutput";
+            this.lblOutput.Size = new System.Drawing.Size(0, 13);
+            this.lblOutput.TabIndex = 5;
+            // 
+            // lblInput
+            // 
+            this.lblInput.AutoSize = true;
+            this.lblInput.Location = new System.Drawing.Point(278, 40);
+            this.lblInput.Name = "lblInput";
+            this.lblInput.Size = new System.Drawing.Size(0, 13);
+            this.lblInput.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(726, 211);
+            this.Controls.Add(this.lblInput);
+            this.Controls.Add(this.lblOutput);
+            this.Controls.Add(this.btnExcelLoc);
+            this.Controls.Add(this.btnPDFLoc);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "FUEL PDF REPORT TO EXCEL";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnPDFLoc;
+        private System.Windows.Forms.Button btnExcelLoc;
+        private System.Windows.Forms.Label lblOutput;
+        private System.Windows.Forms.Label lblInput;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using FastExcel;
+
+
+namespace PDF2CSV
+{
+    
+    public partial class Form1 : Form
+    {
+        string sFileName = null;
+        string sSelectedPath = null;
+        EventLog eventLog;
+
+        public Form1()
+        {
+            InitializeComponent();
+            eventLog = new EventLog("Application");
+            
+                eventLog.Source = "Application";
+                eventLog.WriteEntry("PDF2CSV Started", EventLogEntryType.Information, 101, 1);
+                lblInput.Text = "";
+                lblOutput.Text = "";
+
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            if (!(lblInput.Text == "" && lblOutput.Text == ""))
+            {
+
+                var templateFile = new FileInfo(@".\Template.xlsx");
+                var outputFile = new FileInfo(sSelectedPath + "\\output.xlsx");
+
+                var arlist = new ArrayList();
+                var lineCount = 0;
+                string fileName = sFileName;
+
+
+                //Create a worksheet with some rows
+                var worksheet = new Worksheet();
+                var rows = new List<Row>();
+                // List<Cell> cells = new List<Cell>();
+
+                using (StreamReader reader = new StreamReader(fileName))
+                {
+                    string line;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        lineCount++;
+                        if (lineCount < 89)
+                        {
+                            //Ignore the first 89 Lines
+
+                        }
+                        else
+
+                        {
+
+                            int index = line.IndexOf(@"(");
+                            int index2 = line.LastIndexOf(@")");
+
+                            try
+                            {
+                                if (line.Contains("re S") && line.Contains("Td"))
+                                {
+
+
+                                    arlist.Add(line.Substring(index + 1, (index2 - index) - 1));
+
+                                }
+                                else if (line.Contains("re S"))
+                                {
+                                    arlist.Add("");
+
+                                }
+
+                            }
+
+                            catch (Exception a)
+                            {
+
+                                Console.WriteLine("SKIP");
+                                eventLog.WriteEntry(a.ToString(), EventLogEntryType.Error, 101, 1);
+
+                            }
+
+
+
+
+
+                        }
+
+                    }
+                }
+            
+        
+
+            Console.WriteLine("Total arraycount: "+arlist.Count);
+            Console.WriteLine("Total Lines to write: " +arlist.Count / 8);
+
+            Console.WriteLine("ARRAYLIST: "+arlist.Count);
+            int Original_ArrayCount = arlist.Count;
+
+
+            for (int rowNumber = 1; rowNumber < ((Original_ArrayCount / 8) + 1); rowNumber++)
+            {
+
+                List<Cell> cells = new List<Cell>();
+                for (int counter = 1; counter < 9; counter++)
+                {
+                    
+                        
+                        if(arlist.Count == 0)
+                        {
+                        cells.Add(new Cell(counter, "END"));
+                        }
+                        else
+                        {
+                        //Add the first item into the cell of the worksheet and pop it from the list. 
+                        //The next in the array will be the first [0] and so on and so fourth
+                        //FILO concept.
+                        cells.Add(new Cell(counter, arlist[0]));
+                        arlist.RemoveAt(0);
+                        }
+
+
+
+
+                }
+
+                rows.Add(new Row(rowNumber, cells));
+            }
+
+            worksheet.Rows = rows;
+
+
+            // Create an instance of FastExcel
+            
+                if (File.Exists(sSelectedPath + "\\output.xlsx"))
+                {
+                    File.Delete(sSelectedPath + "\\output.xlsx");
+                }
+
+            using (FastExcel.FastExcel fastExcel = new FastExcel.FastExcel(templateFile, outputFile))
+            {
+                // Write the data
+                fastExcel.Write(worksheet, "Template");
+            }
+
+            Console.WriteLine(arlist.Count);
+
+            MessageBox.Show("File has been converted successfully. Please exit application.");
+
+            }
+            else
+            {
+                MessageBox.Show("Select input and output files.");
+            }
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            sFileName = null;
+            OpenFileDialog choofdlog = new OpenFileDialog();
+            choofdlog.Filter = "All Files (*.*)|*.*";
+            choofdlog.FilterIndex = 1;
+            choofdlog.Multiselect = true;
+
+            if (choofdlog.ShowDialog() == DialogResult.OK)
+            {
+                sFileName = choofdlog.FileName;
+                //string[] arrAllFiles = choofdlog.FileNames; //used when Multiselect = true         
+                lblInput.Text = sFileName;
+            }
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            sSelectedPath = null;
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            fbd.Description = "Custom Description";
+
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+                sSelectedPath = fbd.SelectedPath;
+                lblOutput.Text = sSelectedPath;
+            }
+        }
+    }
+}
